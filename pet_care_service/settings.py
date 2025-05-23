c4a71e20d6c1aac0a351309811bbeee1372894ac
Valeriia Nedashkivska <nedashkivskavaleriia@gmail.com>
2025-05-23 21:38:21 +0300

    Update access token

--- pet_care_service/settings.py
+++ pet_care_service/settings.py
@@ -101,7 +101,7 @@ PASSWORD_HASHERS = [
 ]
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=1),
+    'ACCESS_TOKEN_LIFETIME': timedelta(hours=2),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
     'ROTATE_REFRESH_TOKENS': False,
     'BLACKLIST_AFTER_ROTATION': False
